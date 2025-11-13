0311a11f4e98a88710d4ad8e28a69c2126f68c2f
J-za <jzarrouk@gmail.com>
2025-11-13 18:39:48 +0100

    feat: create Hero section for homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
+import "./home.scss";
+
 function Home() {
   return (
-    <div className="home">
-      <h2>Mes compétences</h2>
-      <p>Bienvenue sur mon portfolio de développeur !</p>
-    </div>
+    <>
+      <section className="hero-content">
+        <h1>
+          Développeur Web <br /> <span class="accent">Full-Stack</span>
+        </h1>
+        <p>
+          Je développe des <strong>applications web modernes</strong>,
+          performantes et élégantes, pensées pour offrir une{" "}
+          <strong>expérience utilisateur fluide et intuitive</strong>
+        </p>
+        <div className="button-content">
+          <button>Mes projets</button>
+          <button>Me contacter</button>
+        </div>
+      </section>
+    </>
   );
 }
 
