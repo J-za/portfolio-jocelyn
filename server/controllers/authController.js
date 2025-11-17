a6bb79a3239352ea52a11a1e88720313ebb4f7a5
J-za <jzarrouk@gmail.com>
2025-11-17 09:58:49 +0100

    refactor: remove signup route and controller to enforce single-account policy
    - Deleted signup route and controller to prevent creation of new accounts by third parties
    - Enforced single-account policy (only owner account remains accessible)
    - Cleaned up related backend logic and references to signup functionality
    - Updated documentation and code comments to reflect removal of account creation feature

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,26 +3,6 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
-exports.signup = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(409).json({ error: "Email already in use" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = new User({ email, password: hashedPassword });
-    await user.save();
-
-    res.status(201).json({ message: "User created" });
-  } catch (error) {
-    res.status(400).json({ error: "Signup failed", details: error.message });
-  }
-};
-
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
