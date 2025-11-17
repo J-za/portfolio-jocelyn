062a1d23c91bf8c9646a9ed1242d34e228daba59
J-za <jzarrouk@gmail.com>
2025-11-17 15:03:05 +0100

    feat: add ScrollToTop component to fix React page change bug
    - Created reusable ScrollToTop component to reset scroll position on route change
    - Integrated component into routing logic to ensure consistent behavior across pages
    - Fixed known React bug where scroll position persisted when navigating between routes
    - Improved user experience by automatically scrolling to top on page transitions

--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -2,15 +2,18 @@ import { Routes, Route } from "react-router";
 import Home from "../pages/Home/Home";
 import Layout from "../components/Layout/Layout";
 import About from "../pages/About/About";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 function Router() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/a-propos" element={<About />} />
-      </Route>
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/a-propos" element={<About />} />
+        </Route>
+      </Routes>
+    </ScrollToTop>
   );
 }
 
