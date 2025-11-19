08217fd2aa51bb3b25b0834488fdcc14ec2c5978
J-za <jzarrouk@gmail.com>
2025-11-19 21:17:45 +0100

    fix: add missing cors import in backend

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
+const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
