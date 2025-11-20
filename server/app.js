2edaa19a88756eb8c7178337163f9e43375899e1
J-za <jzarrouk@gmail.com>
2025-11-20 18:38:36 +0100

    fix: correct frontend production URL in CORS configuration

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://portfolio-jocelyn.vercel.app/"],
+    origin: ["http://localhost:5173", "https://portfolio-jocelyn.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
       "Origin",
