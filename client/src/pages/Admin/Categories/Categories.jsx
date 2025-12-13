bcd74ca439aa6555bfaa45068e7a85a8cdeebbca
J-za <jzarrouk@gmail.com>
2025-12-13 19:15:16 +0100

    ix: correct typo in categories.scss and skills.scss import causing deployment error

--- client/src/pages/Admin/Categories/Categories.jsx
+++ client/src/pages/Admin/Categories/Categories.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import "./Categories.scss";
+import "./categories.scss";
 import { API_URL } from "../../../config/api";
 
 function Categories() {
--- client/src/pages/Admin/Skills/Skills.jsx
+++ client/src/pages/Admin/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import "./Skills.scss";
+import "./skills.scss";
 import { API_URL } from "../../../config/api";
 
 function Skills() {
